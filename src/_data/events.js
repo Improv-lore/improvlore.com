3f25da9b53f3ff44b244346352c51e2786726542
Mohit <mgumber909@gmail.com>
2026-01-03 12:18:32 +0530

    Refactor event data processing to include custom data from custom.json

--- src/_data/events.js
+++ src/_data/events.js
@@ -4,16 +4,8 @@ import fs from "fs";
 
 export default async function() {
   const transformed = await transformCalendar(await rawCalendar());
-  let currentDataString = "";
-  if (fs.existsSync("output.json")) {
-    currentDataString = fs.readFileSync('output.json', "utf8");
-  }
-  if (currentDataString !== transformed) {
-  console.log("Data changed. Updating output.json...");
-  // write local file
-  fs.writeFileSync("output.json", JSON.stringify(transformed, null, 4));
-  } else {
-    console.log("Data unchanged. No update needed.");
-  }
-  return transformed;
+  const rawCustom = await fs.readFile('./custom.json', 'utf-8');
+  const customData = JSON.parse(rawCustom);
+  const finalData= transformed.concat(customData);
+  return finalData;
 }
