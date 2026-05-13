b962d428ea81cb0de1d3d0de15670c150edae396
Mohit <mgumber909@gmail.com>
2026-05-13 12:52:50 +0530

    Fix: update event JSON URL to point to the latest release

--- src/_data/events.js
+++ src/_data/events.js
@@ -3,7 +3,7 @@ import eleventyFetch from "@11ty/eleventy-fetch";
 export default async function () {
   try {
     return await eleventyFetch(
-      "https://github.com/heresmohit/UC-ingest/releases/latest/download/events.json",
+      "https://github.com/heresmohit/UC-ingest/releases/download/improvlore-latest/events.json",
       { duration: "1h", type: "json" }
     );
   } catch {
